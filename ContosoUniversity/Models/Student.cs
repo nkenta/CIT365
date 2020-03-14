f7ab9189a7af459e423af83b0ea6e85756813b3e
Uchechukwu Ferdinand Nkenta <uchenkenta@gmail.com>
2020-03-14 23:07:46 +0100

    Contoso University Added

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Display(Name = "Student Last Name")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+        [Display(Name = "Student First Name")]
+        [DataType(DataType.Text)]
+        public string FirstMidName { get; set; }
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
